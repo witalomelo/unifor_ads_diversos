8a164850da2616819f9b4d46679413fa5f12239b
witalomelo <witalovmelo@gmail.com>
2023-03-23 17:12:02 -0300

    commit

--- algoritmos-js/Fibonacci.js
+++
@@ -1,8 +0,0 @@
-var num = parseInt(prompt("quantos numeros quero mostrar ?"));
-
-var array = [0,1];
-
-for (let i = 2; i < num; i++) {
-  array[i] = array[i - 1] + array[i - 2];
-}
-document.write( array.join(","));
